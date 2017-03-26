993b73e0445af612058185eb46a58fea4bcc8c6f
An Luu <lta.anluu@gmail.com>
2017-03-26 12:11:20 +0700

    increase, decrease quantity button in item bag

--- javascripts/component.js
+++ javascripts/component.js
@@ -2,3 +2,28 @@ function showFullArticle() {
   $('.box-article').attr('style', "height: ''");
   $('.btn-show-more').addClass('hidden');
 }
+
+function increaseQuantity(form) {
+  var input = form.getElementsByTagName('input')[0];
+
+  var value = input.value;
+
+  if (value == '') {
+    input.value = 0;
+  } else {
+    input.value = parseInt(value) + 1;
+  }
+}
+
+function decreaseQuantity(form) {
+  var input = form.getElementsByTagName('input')[0];
+
+  var value = input.value;
+
+  if (value == '') {
+    input.value = 0;
+  } else {
+    var valueInt = parseInt(value);
+    input.value = (valueInt <= 0) ? 0 : valueInt - 1
+  }
+}
